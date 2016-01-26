62984e61fd6400c41dbf247c04b322794069ad1d
Joel Ross <joelross@uw.edu>
2016-01-26 12:25:44 -0800

    End of lecture code

--- app/src/main/java/edu/uw/intentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/intentdemo/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uw.intentdemo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "**DEMO**";
 
+    private static final int REQUEST_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v(TAG, "Launch button pressed");
 
+                //Explicit intent
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("edu.uw.intentdemo.message", "Hello number 2!");
+                startActivity(intent);
 
             }
         });
@@ -36,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v(TAG, "Call button pressed");
 
+                //implicit intent
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("tel:206-685-1622"));
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
 
             }
         });
@@ -46,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v(TAG, "Camera button pressed");
 
-
+                //implicit intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_PICTURE);
+                }
             }
         });
 
@@ -62,5 +79,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        //if camera responds with a "yup!"
+        if(requestCode == REQUEST_PICTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap)extras.get("data");
+
+            ((ImageView)findViewById(R.id.imgThumbnail)).setImageBitmap(bitmap);
+
+        }
+
+    }
 }
